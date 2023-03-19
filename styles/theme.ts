53a4ce131dac6a09053f92bab86fb0d83425cf67
adamkubina1 <adam.kubinaa@gmail.com>
2023-03-19 18:08:27 +0100

    Added color scheme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,5 +1,16 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const theme = extendTheme({});
+const colors = {
+  swap: {
+    green: '#00887A',
+    pink: '#FFCCBC',
+    lightBlue: '#D3E3FC',
+    blue: '#77A6f7',
+    black: '#030201',
+    white: '#FFFFFF',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 export { theme };
