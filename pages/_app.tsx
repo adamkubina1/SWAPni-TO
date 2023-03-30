33548a00a32877d4f86a7f5108501ef050b430f0
adamkubina1 <adam.kubinaa@gmail.com>
2023-03-30 20:59:40 +0200

    Refactored authProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,27 @@
 import FirebaseAppCheckProvider from '@/components/context/FirebaseAppCheckProvider';
+import FirebaseAuthProvider from '@/components/context/FirebaseAuthProvider';
 import { DefaultLayout } from '@/components/layout/DefaultLayout';
 import '@/styles/global.css';
 import { theme } from '@/styles/theme';
 import { firebaseConfig } from '@/utils/firebaseConfig';
 import { ChakraProvider } from '@chakra-ui/react';
-import { initializeApp } from '@firebase/app';
-import { isBrowser } from '@firebase/util';
-import {
-  indexedDBLocalPersistence,
-  initializeAuth,
-  inMemoryPersistence,
-} from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 import type { AppProps } from 'next/app';
-import { AuthProvider, FirebaseAppProvider } from 'reactfire';
+import { FirebaseAppProvider } from 'reactfire';
 
 export default function App({ Component, pageProps }: AppProps) {
   const app = initializeApp(firebaseConfig);
 
-  // indexedDB is not supported on server
-  const persistence = isBrowser()
-    ? indexedDBLocalPersistence
-    : inMemoryPersistence;
-
-  const auth = initializeAuth(app, { persistence });
-
   return (
     <FirebaseAppProvider firebaseApp={app} suspense={true}>
       <FirebaseAppCheckProvider>
-        <AuthProvider sdk={auth}>
+        <FirebaseAuthProvider>
           <ChakraProvider theme={theme}>
             <DefaultLayout>
               <Component {...pageProps} />
             </DefaultLayout>
           </ChakraProvider>
-        </AuthProvider>
+        </FirebaseAuthProvider>
       </FirebaseAppCheckProvider>
     </FirebaseAppProvider>
   );
