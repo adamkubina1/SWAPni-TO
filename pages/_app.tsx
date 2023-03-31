cd565c76ee8401117f764c6d016e0a922a9452e6
adamkubina1 <adam.kubinaa@gmail.com>
2023-03-31 14:17:17 +0200

    Added storage provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,22 +7,26 @@ import { theme } from '@/styles/theme';
 import { firebaseConfig } from '@/utils/firebaseConfig';
 import { ChakraProvider } from '@chakra-ui/react';
 import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
 import type { AppProps } from 'next/app';
-import { FirebaseAppProvider } from 'reactfire';
+import { FirebaseAppProvider, StorageProvider } from 'reactfire';
 
 export default function App({ Component, pageProps }: AppProps) {
   const app = initializeApp(firebaseConfig);
+  const storage = getStorage(app);
 
   return (
     <FirebaseAppProvider firebaseApp={app}>
       <FirebaseAppCheckProvider>
         <FirebaseAuthProvider>
           <FirebaseFirestoreProvider>
-            <ChakraProvider theme={theme}>
-              <DefaultLayout>
-                <Component {...pageProps} />
-              </DefaultLayout>
-            </ChakraProvider>
+            <StorageProvider sdk={storage}>
+              <ChakraProvider theme={theme}>
+                <DefaultLayout>
+                  <Component {...pageProps} />
+                </DefaultLayout>
+              </ChakraProvider>
+            </StorageProvider>
           </FirebaseFirestoreProvider>
         </FirebaseAuthProvider>
       </FirebaseAppCheckProvider>
