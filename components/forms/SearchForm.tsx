a8563c8b304c3085e704a3f2e4277bfae30c06c5
adamkubina1 <adam.kubinaa@gmail.com>
2023-04-01 16:19:34 +0200

    Added search and api call

--- components/forms/SearchForm.tsx
+++ components/forms/SearchForm.tsx
@@ -1,19 +1,38 @@
 import { ValidateSearch } from '@/lib/formValidators';
+import { SearchType } from '@/lib/types/Search';
 import { Box, Button, InputRightAddon, Stack } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
+import { Dispatch, SetStateAction } from 'react';
 import { MdSearch } from 'react-icons/md';
 import { FormFieldInput, FormFieldSelect } from './FormField';
 
-const searchOptions: Array<{ value: string; description: string }> = [
-  { value: 'searchBookName', description: 'Knihu z názvu' },
+const searchOptions: Array<{ value: SearchType; description: string }> = [
+  { value: 'searchBookName', description: 'Knihu název/ISBN' },
   { value: 'searchUser', description: 'Uživatele' },
 ];
 
-const SearchForm = () => {
+const SearchForm = ({
+  setSearch,
+  setSearchType,
+}: {
+  setSearch: Dispatch<SetStateAction<string>>;
+  setSearchType: Dispatch<SetStateAction<SearchType>>;
+}) => {
   const onSubmitForm = async (values: {
     search: string;
     searchType: string;
-  }) => {};
+  }) => {
+    switch (values.searchType) {
+      case 'searchBookName':
+        setSearch(values.search);
+        setSearchType('searchBookName');
+        break;
+      case 'searchUser':
+        setSearch(values.search);
+        setSearchType('searchUser');
+        break;
+    }
+  };
 
   return (
     <Formik
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import { SearchForm } from '@/components/forms/SearchForm';
 import NoSSR from '@/components/NoSSR';
 import { Seo } from '@/components/Seo';
-import { Heading, VStack } from '@chakra-ui/react';
+import { SearchType } from '@/lib/types/Search';
+import { Heading, Spinner, Text, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
+import useSWR from 'swr';
 
 const Home = () => {
+  const [search, setSearch] = useState<string>('');
+  const [searchType, setSearchType] = useState<SearchType>('none');
+
   return (
     <>
       <Seo />
@@ -12,11 +18,43 @@ const Home = () => {
           Prozkoumejte SWAPni TO
         </Heading>
         <NoSSR>
-          <SearchForm />
+          <SearchForm setSearch={setSearch} setSearchType={setSearchType} />
+          <SearchResult search={search} searchType={searchType} />
         </NoSSR>
       </VStack>
     </>
   );
+};
+
+const SearchResult = ({
+  search,
+  searchType,
+}: {
+  search: string;
+  searchType: SearchType;
+}) => {
+  if (searchType === 'none') return <></>;
+
+  if (searchType === 'searchBookName')
+    return <SearchBookByNameResults search={search} />;
+
+  if (searchType === 'searchUser') return <> User {search}</>;
+  return <></>;
+};
+
+const SearchBookByNameResults = ({ search }: { search: string }) => {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading, isValidating } = useSWR(
+    `https://www.googleapis.com/books/v1/volumes?q=${search}`,
+    fetcher
+  );
+
+  if (isLoading || isValidating) return <Spinner />;
+  if (error) return <Text>Nebyly nelezeny žádné výsledky.</Text>;
+
+  console.log(data);
+
+  return <></>;
 };
 
 export default Home;
